a40e4e19741de6995a4877098198e0ef7bbaaa88
Vorchalov Konstantin <Konstantin.Vorchalov@metran.ru>
2025-05-27 14:05:49 +0300

    vite fixes

--- template-manager/vite.config.js
+++ template-manager/vite.config.js
@@ -5,7 +5,7 @@ import vue from "@vitejs/plugin-vue";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [vue()],
-  base: process.env.NODE_ENV === 'production' ? '/AiScreenTemplates/' : '/',
+  base: '/AiScreenTemplates/',
   server: {
     proxy: {
       "/api": {
